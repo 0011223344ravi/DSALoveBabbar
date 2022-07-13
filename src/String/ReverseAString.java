98c45f217455154a9d916f6e8fd5af5fc32873f6
Ravi kumar gupta <0011223344ravi@gmail.com>
2022-07-13 20:46:26 +0530

    reverseStringCommit

--- src/String/ReverseAString.java
+++ src/String/ReverseAString.java
@@ -3,9 +3,24 @@ package String;
 import java.util.Scanner;
 
 public class ReverseAString {
+    public static String reverseWord(String str)
+    {
+        String reversedString = "";
 
-    Scanner sc = new Scanner(System.in);
-    String str =sc.nextLine();
+        int n = str.length();
+        char arr [] = new char[n];
+        arr=  str.toCharArray();
+        for(int i =n-1 ; i>=0; i--){
+            reversedString = reversedString+arr[i];
 
-    
+        }
+        return reversedString;
+    }
+    public static void main (String args []) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(reverseWord(str));
+
+    }
+
 }
