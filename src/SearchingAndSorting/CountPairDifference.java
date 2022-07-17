99106a7f02540fdd307a9e27edc18e333bb1a6a0
Ravi kumar gupta <0011223344ravi@gmail.com>
2022-07-17 20:59:52 +0530

    Create CountPairDifference.java

---
+++ src/SearchingAndSorting/CountPairDifference.java
@@ -0,0 +1,23 @@
+package SearchingAndSorting;
+
+public class CountPairDifference {
+
+     public boolean findPair(int arr[], int size, int n)
+     {
+           boolean flag =false;
+
+          for (int i = 0; i < n; i++)
+               for (int j = i + 1; j < n; j++)
+                    if (n-arr[])
+
+
+
+
+           return flag;
+     }
+
+     public static void main (String args []){
+
+
+     }
+}
